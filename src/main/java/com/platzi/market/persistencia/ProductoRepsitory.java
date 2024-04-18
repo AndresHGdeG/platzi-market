891469eb111a920dbc5f2cf65a872a0b4dbacf0a
AndresHGdeG <a.h.mx@outlook.com>
2024-04-17 21:01:36 -0600

    Query Methods
    En este ejemplo se implementaron 2 query methods
    utilizando spring JPA

--- src/main/java/com/platzi/market/persistencia/ProductoRepsitory.java
+++ src/main/java/com/platzi/market/persistencia/ProductoRepsitory.java
@@ -4,11 +4,20 @@ import com.platzi.market.persistencia.crud.ProductoCrudRepository;
 import com.platzi.market.persistencia.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepsitory {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> findAll() {
         return (List<Producto>)productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
